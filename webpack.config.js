1fbde8b7d9129de8dc32ef4c14df0353c8be4d96
Joe C <joe.chamberlain@silverstripe.com>
2023-10-05 16:00:39 +1300

    Added styles to embargo badge
    Updated webpack.config, added styles, added extension to inject styles to left and main.

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,55 @@
 const Path = require('path');
-const webpack = require('webpack');
-const { JavascriptWebpackConfig, CssWebpackConfig } = require('@silverstripe/webpack-config');
+const webpackConfig = require('@silverstripe/webpack-config');
+const {
+  resolveJS,
+  externalJS,
+  moduleJS,
+  pluginJS,
+  moduleCSS,
+  pluginCSS,
+} = webpackConfig;
 
+const ENV = process.env.NODE_ENV;
 const PATHS = {
+  MODULES: 'node_modules',
+  FILES_PATH: '../',
   ROOT: Path.resolve(),
-  SRC: Path.resolve(),
+  SRC: Path.resolve('client/src'),
+  DIST: Path.resolve('client/dist'),
 };
 
 const config = [
-  new JavascriptWebpackConfig('js', { ...PATHS, DIST: `${PATHS.ROOT}/client/dist/js/` })
-    .setEntry({
-      main: 'client/src/bundles/embargo-expiry.js'
-    })
-    // Output the javascript with a different filename schema than the default
-    .mergeConfig({
-      output: {
-        filename: 'embargo-expiry.js',
-      },
-    })
-    .getConfig(),
+  {
+    name: 'js',
+    entry: {
+      embargoExpiry: `${PATHS.SRC}/bundles/embargo-expiry.js`,
+    },
+    output: {
+      path: PATHS.DIST,
+      filename: 'js/embargo-expiry.js',
+    },
+    devtool: (ENV !== 'production') ? 'source-map' : '',
+    resolve: resolveJS(ENV, PATHS),
+    externals: externalJS(ENV, PATHS),
+    module: moduleJS(ENV, PATHS),
+    plugins: pluginJS(ENV, PATHS),
+  },
+  {
+    name: 'css',
+    entry: {
+      main: `${PATHS.SRC}/styles/embargo.scss`
+    },
+    output: {
+      path: PATHS.DIST,
+      filename: 'mains.css'
+    },
+    devtool: (ENV !== 'production') ? 'source-map' : '',
+    module: moduleCSS(ENV, PATHS),
+    plugins: pluginCSS(ENV, PATHS),
+  },
 ];
 
-module.exports = config;
+// Use WEBPACK_CHILD=js or WEBPACK_CHILD=css env var to run a single config
+module.exports = (process.env.WEBPACK_CHILD)
+  ? config.find((entry) => entry.name === process.env.WEBPACK_CHILD)
+  : module.exports = config;
